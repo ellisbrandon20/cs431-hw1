5e2b28e0ba6bf06af722c26bc495dfdcc08a0ee1
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-01-29 00:24:59 +0000

    part 2 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -17,15 +17,34 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    false
+  else
+    s = s.downcase
+    if !s[0].match(/[a,e,i,o,u]/) && s[0].match(/^[[:alpha:]]$/)
+      true
+    else
+      false
+    end
+  end
+  
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # Define a method binary_multiple_of_4?(s) that takes a string and 
+  # returns true if the string represents a binary number that is a multiple of 4. 
+  # NOTE: be sure it returns false if the string is not a valid binary number! 
+
+  return false if s.empty? # empty string case
+  return false if s.match(/[a-zA-Z]/) # invalid binary number
+  
+  int_s = s.to_i(10) # convert to int base 10
+  return int_s % 2 == 0
+  
 end
 
 # Part 3
